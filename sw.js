3b83aa70b74d798c5d8003d0277c83f184a29a6f
zxsla <jierryx9@gmail.com>
2026-04-07 20:22:06 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "blue-note-pro-v1";
+const FILES = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
